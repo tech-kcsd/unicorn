5900614158d5b50a31c4383a0085b80a01614677
Сейтказин Б.М <SeitkazinB@csdepo.net>
2022-07-27 16:10:54 +0600

    first commit

---
+++ src/main/java/kz/kcsd/unicorn/UnicornApplication.java
@@ -0,0 +1,13 @@
+package kz.kcsd.unicorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicornApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/kcsd/unicorn/UnicornApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.kcsd.unicorn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
